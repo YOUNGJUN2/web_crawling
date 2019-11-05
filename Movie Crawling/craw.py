5ebbbbd720d9f879a66e8045b66e0fce85ee98ad
YJUNY <36507979+YOUNGJUN2@users.noreply.github.com>
2019-11-05 14:33:25 +0900

    Add files via upload

---
+++ Movie Crawling/craw.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import parser2
+
+j = 1
+
+name = input("영화제목  입력: ")
+
+name = parser2.par(name)
+req = requests.get(name)
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+tag = soup.findAll('a', attrs={'class':'go_report2'})
+tag2 = soup.findAll('span', attrs={'class':'st_on'})
+
+tag = str(tag)
+tag2 = str(tag2)
+
+p = re.search('.*(\d.\d\d).*', tag2)
+p = p.group(1)
+
+print("\n\n관람객 평점:",p)
+print("")
+
+han = re.compile('[^ ㄱ-ㅣ가-힣]+')
+p = han.sub('', tag)
+
+hana = re.compile('신고')
+p = hana.sub('', p)
+
+l = p.split("      ")
+
+print("상위 5개 댓글")
+
+for i in l:
+    print(j,".",i)
+    j+=1
+
+
+
---
+++ Movie Crawling/parser2.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import urllib.request
+from sys import argv
+import re
+
+def par(name):
+    client_id = "5dySw1LrJcS06zuSLPsC"
+    client_secret = "70xfw7XbKI"
+    encText = urllib.parse.quote(name)
+    url = "https://openapi.naver.com/v1/search/movie?query=" + encText # json 결과
+    #url = "https://openapi.naver.com/v1/search/movie.xml?query=" + encText # xml 결과
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+    if(rescode==200):
+            response_body = response.read()
+            u = response_body.decode('utf-8')
+        
+            p = re.search('(https\w*\D*\d*)', u)
+            p = p.group(1)
+    else:
+            print("Error Code:" + rescode)
+    
+    return p
+
